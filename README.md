8444448b8b9e0db5434bedf4b78e5741a75f4eb0
Stevica Knezevic <knezevic.steva@gmail.com>
2016-06-12 15:24:36 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#ANGULAR 2 STARTER KIT
+
+###Install starter kit
+
+`npm install`
---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>Angular 2 App</h1>'
+})
+
+export class AppComponent {
+}
